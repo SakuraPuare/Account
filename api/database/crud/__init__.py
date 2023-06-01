910b687c8234da58b4daaeae3301021d41cb6c72
Steven Moder <java20131114@gmail.com>
2023-06-01 12:23:44 +0800

    🦄 refactor(api): Database Operation

--- api/database/crud/__init__.py
+++ api/database/crud/__init__.py
@@ -1,36 +1,35 @@
 # coding=utf-8
-# from sqlalchemy.orm import Session
-#
-# from database import models, schemas
-#
-# def get_user(db: Session, user_id: int):
-#     return db.query(models.User).filter(models.User.id == user_id).first()
-#
-#
-# def get_user_by_email(db: Session, email: str):
-#     return db.query(models.User).filter(models.User.email == email).first()
-#
-#
-# def get_users(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.User).offset(skip).limit(limit).all()
-#
-#
-# def create_user(db: Session, user: schemas.UserCreate):
-#     fake_hashed_password = user.password + "notreallyhashed"
-#     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
-#     db.add(db_user)
-#     db.commit()
-#     db.refresh(db_user)
-#     return db_user
-#
-#
-# def get_items(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.Item).offset(skip).limit(limit).all()
-#
-#
-# def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-#     db_item = models.Item(**item.dict(), owner_id=user_id)
-#     db.add(db_item)
-#     db.commit()
-#     db.refresh(db_item)
-#     return db_item
+from sqlalchemy.orm import Session
+
+from database import models
+
+
+def create(db: Session, model: models.Base) -> models.Base:
+    try:
+        db.add(model)
+        db.commit()
+        db.refresh(model)
+    except Exception as e:
+        db.rollback()
+        raise e
+    return model
+
+
+def update(db: Session, model: models.Base) -> models.Base:
+    try:
+        db.commit()
+        db.refresh(model)
+    except Exception as e:
+        db.rollback()
+        raise e
+    return model
+
+
+def delete(db: Session, model: models.Base) -> None:
+    try:
+        db.delete(model)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise e
+    return None
--- api/database/crud/user/__init__.py
+++ api/database/crud/user/__init__.py
@@ -4,18 +4,12 @@ import datetime
 from sqlalchemy.orm import Session
 
 from database import models, schemas
+from database.crud import create, update, delete
 from utils.encryption import generate_user_token, generate_hash_password, generate_md5, generate_captcha_code
 
 
 def create_user(db: Session, user: models.User) -> models.User:
-    try:
-        db.add(user)
-        db.commit()
-        db.refresh(user)
-    except Exception as e:
-        db.rollback()
-        raise e
-    return user
+    return create(db, user)
 
 
 def get_user_by_id(db: Session, user_id: int) -> models.User:
@@ -35,35 +29,29 @@ def get_user_by_email(db: Session, email: str) -> models.User:
 
 
 def update_user(db: Session, user: models.User) -> models.User:
-    try:
-        user.updated_at = datetime.datetime.now()
-        db.commit()
-        db.refresh(user)
-    except Exception as e:
-        db.rollback()
-        raise e
-    return user
+    user.updated_at = datetime.datetime.now()
+    return update(db, user)
 
 
-def update_user_info(db: Session, update: schemas.UserUpdate, current: models.User) -> models.User:
-    if update.slogan:
-        current.slogan = update.slogan
-    if update.username:
-        current.username = update.username
-    return update_user(db, current)
+def update_user_info(db: Session, updated_user: schemas.UserUpdate, current_user: models.User) -> models.User:
+    if updated_user.slogan:
+        current_user.slogan = updated_user.slogan
+    if updated_user.username:
+        current_user.username = updated_user.username
+    return updated_user(db, current_user)
 
 
-def update_user_password(db: Session, update: schemas.UserUpdate, current: models.User) -> models.User:
-    current.encrypted_password = generate_hash_password(update.new_password)
-    return update_user(db, current)
+def update_user_password(db: Session, updated_user: schemas.UserUpdate, current_user: models.User) -> models.User:
+    current_user.encrypted_password = generate_hash_password(updated_user.new_password)
+    return update_user(db, current_user)
 
 
-def update_user_email(db: Session, update: schemas.UserUpdate, current: models.User) -> models.User:
+def update_user_email(db: Session, update_user: schemas.UserUpdate, current_user: models.User) -> models.User:
     # update email
-    current.email = update.email
-    current.email_md5 = generate_md5(update.email)
-    current.email_verified_at = None
-    return update_user(db, current)
+    current_user.email = update_user.email
+    current_user.email_md5 = generate_md5(update_user.email)
+    current_user.email_verified_at = None
+    return update_user(db, current_user)
 
 
 def update_user_email_captcha(db: Session, user: models.User) -> models.User:
@@ -75,14 +63,7 @@ def create_user_token(db: Session, user: models.User, lifetime: int = 86400) -> 
     token = generate_user_token(user)
 
     db_token = models.UserToken(user_id=user.id, token=token, lifetime=lifetime)
-    try:
-        db.add(db_token)
-        db.commit()
-        db.refresh(db_token)
-    except Exception as e:
-        db.rollback()
-        raise e
-    return db_token
+    return create(db, db_token)
 
 
 def create_user_token_from_user_id(db: Session, user_id: int, lifetime: int) -> models.UserToken:
@@ -105,29 +86,18 @@ def get_user_token_by_token(db: Session, token: str) -> models.UserToken:
 
 
 def delete_user_token(db: Session, token: models.UserToken) -> None:
-    try:
-        db.delete(token)
-        db.commit()
-    except Exception as e:
-        db.rollback()
-        raise e
+    if token:
+        return delete(db, token)
 
 
 def delete_user_token_by_id(db: Session, ids: int) -> None:
     token = get_user_token_by_id(db, ids)
-    if token:
-        delete_user_token(db, token)
+    return delete_user_token(db, token)
 
 
 def delete_user_token_by_token(db: Session, token: str) -> None:
     db_token = get_user_token_by_token(db, token)
-    if db_token:
-        try:
-            db.delete(db_token)
-            db.commit()
-        except Exception as e:
-            db.rollback()
-            raise e
+    return delete_user_token(db, db_token)
 
 
 def create_email_captcha(db: Session, user: models.User) -> None:
@@ -135,22 +105,12 @@ def create_email_captcha(db: Session, user: models.User) -> None:
 
     captcha = models.EmailCaptcha(user_id=user.id, email=user.email, captcha=code)
 
-    try:
-        db.add(captcha)
-        db.commit()
-        db.refresh(captcha)
-    except Exception as e:
-        db.rollback()
-        raise e
+    return create(db, captcha)
 
 
 def delete_email_captcha(db: Session, captcha: models.EmailCaptcha) -> None:
-    try:
-        db.delete(captcha)
-        db.commit()
-    except Exception as e:
-        db.rollback()
-        raise e
+    if captcha:
+        return delete(db, captcha)
 
 
 def update_email_captcha(db: Session, user: models.User, captcha: schemas.EmailCaptchaUpdate) -> bool:
