ee2d7f26abb605cfe144ce280860b7fd64c46cf1
Steven Moder <java20131114@gmail.com>
2023-06-04 12:13:58 +0800

    ✨ feat(web): Component

---
+++ web/src/components/Footer/FooterComp.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="pb-8 w-full align-bottom inline-block">
+    <hr class="mx-10 mt-4 border-gray-300" />
+    <div class="mt-6 text-center text-gray-500 text-sm">
+      <p class="text-sm pb-4">这是一句一言</p>
+
+      <p>Made by 书樱寄语 with ❤️</p>
+      <p>&copy; 2020 - 2023 All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<script setup></script>
---
+++ web/src/components/Header/HeaderComp.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="bg-pink-400 select-none">
+    <div class="p-4 h-16 flex flex-row items-center content-center justify-evenly">
+      <div class="h-12 w-12 items-center rounded-full outline-1">
+        <button class="h-12 w-12 mx-3 items-center flex">
+          <MenuIcon />
+        </button>
+      </div>
+      <div>
+        <h1 class="mx-4 font-semibold text-xl">Account</h1>
+      </div>
+      <div class="grow"></div>
+      <div class="items-center">
+        <img
+          alt="Profile image"
+          class="h-12 w-12 rounded-full shadow object-cover ring ring-pink-500 hover:ring-pink-300"
+          src="src/assets/1.png"
+        />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import MenuIcon from 'vue-material-design-icons/Menu.vue'
+</script>
